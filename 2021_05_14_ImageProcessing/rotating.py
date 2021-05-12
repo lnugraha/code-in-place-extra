50070046c5ab50e21da4c73cec79e18b17b6ba37
lnugraha <leo.nugraha@outlook.com>
2021-05-12 22:55:57 +0800

    Added image rotation method

--- 2021_05_14_ImageProcessing/rotating.py
+++ 2021_05_14_ImageProcessing/rotating.py
@@ -12,10 +12,13 @@ def main():
 
     # 2. Image rotation here; assume rotate by 90 deg or 180 deg or 270 deg
     # Only select those 3 angles to avoid unnecessary image interpolation
+    # a. Rotate  90 degree clockwise: cv2.ROTATE_90_CLOCKWISE
+    # b. Rotate 180 degree clockwise: cv2.ROTATE_180
+    # c. Rotate -90 degree clockwise: cv2.ROTATE_90_COUNTERCLOCKWISE
+    lena_rotated = cv2.rotate(lena, cv2.ROTATE_90_CLOCKWISE) 
     
-
     # 3. Save your modified image
-    cv2.imwrite("./results/lena_rotated.png", lena_modified)
+    cv2.imwrite("./results/lena_rotated.png", lena_rotated)
 
 if __name__ == "__main__":
     main()
